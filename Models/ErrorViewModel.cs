62d69d066773fd150823b3a38198c98016831ab4
devpresss xmont <xmontemorjerald@gmail.com>
2024-08-01 15:22:13 +0800

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_HANDS_ON_3RDYEAR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
