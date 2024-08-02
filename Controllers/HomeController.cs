19b62aa4b23dd530f9ac156fd63b78c39ffd8f98
devpresss xmont <xmontemorjerald@gmail.com>
2024-08-02 15:45:44 +0800

    Loop Exam1 codes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,62 +16,20 @@ namespace ASP_HANDS_ON_3RDYEAR.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            ICollection<int> numbers = new List<int>() {1,2,3,4,5};
+            return View(numbers);
         }
         [HttpPost]
-        public IActionResult Grade(Grade model)
+        public IActionResult Loop(ICollection<int> model)
         {
-            if (ModelState.IsValid)
-            {
-                if (model.grade <= 74)
-                {
-                    ViewBag.Message = "Failed";
-                }
-                else if (model.grade > 74 && model.grade <= 76)
-                {
-                    ViewBag.Message = "3.00";
-                }
-                else if (model.grade > 76 && model.grade <= 77)
-                {
-                    ViewBag.Message = "2.75";
-                }
-                else if (model.grade > 77 && model.grade <= 81)
-                {
-                    ViewBag.Message = "2.50";
-                }
-                else if (model.grade > 81 && model.grade <= 84)
-                {
-                    ViewBag.Message = "2.25";
-                }
-                else if (model.grade > 84 && model.grade <= 88)
-                {
-                    ViewBag.Message = "2.00";
-                }
-                else if (model.grade > 88 && model.grade <= 91)
-                {
-                    ViewBag.Message = "1.75";
-                }
-                else if (model.grade > 91 && model.grade <= 94)             {
-                    ViewBag.Message = "1.50";
-                }
-                else if (model.grade > 94 && model.grade <= 97)
-                {
-   
-                    ViewBag.Message = "1.25";
-                }
-                else if (model.grade > 97 && model.grade <= 100)
-                {
-                    ViewBag.Message = "1.00";
-                }
-                else
-                {
-                    ViewBag.Message = "Invalid grade";
-                }
 
-                ViewBag.Grade = model;
+            if (model == null || !model.Any())
+            {
+                model = new List<int>() { 1, 2, 3, 4, 5 };
             }
 
-            return PartialView("Index", model);
+            var reversedNumbers = model.Reverse().ToList();
+            return View("Index", reversedNumbers);
         }
 
 
